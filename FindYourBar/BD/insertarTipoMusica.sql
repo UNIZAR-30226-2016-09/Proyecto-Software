7e77b5d3098e81170c80fe03f7574365d3dfd8ff
beapc18 <beapc_18@hotmail.com>
2016-03-21 23:18:36 +0100

    BD: Relacional, inserts tablas bar, tipoMusica, hay

---
+++ FindYourBar/BD/insertarTipoMusica.sql
@@ -0,0 +1,7 @@
+INSERT INTO `tipoMusica` (`musica`) VALUES ('Pop/musica comercial');
+INSERT INTO `tipoMusica` (`musica`) VALUES ('Rap');
+INSERT INTO `tipoMusica` (`musica`) VALUES ('Rock/Heavy');
+INSERT INTO `tipoMusica` (`musica`) VALUES ('Latina');
+INSERT INTO `tipoMusica` (`musica`) VALUES ('Indie');
+INSERT INTO `tipoMusica` (`musica`) VALUES ('Electronica');
+INSERT INTO `tipoMusica` (`musica`) VALUES ('Años 60-80');
